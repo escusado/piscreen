a83a0c4edc4ac513d889e8f16063f1efdf47c631
Toily <escusado@gmail.com>
2017-11-12 00:48:10 -0600

    this is it

--- src/app.js
+++ src/app.js
@@ -24,6 +24,8 @@ module.exports = class App {
         this.frame[y][x] = y===0 && x===(Engine.width-1) ? 'green' : this.frame[y][x];
         this.frame[y][x] = y===(Engine.height-1) && x===0 ? 'magenta' : this.frame[y][x];
 
+        this.frame[y][x] = (x%8>0) || (y%8>0) ? 'black' : this.frame[y][x];
+
         // this.frame[y][x] = colorIndex % 24 ? colorScale[colorIndex] : 'white';
         colorIndex++;
         console.log();
@@ -46,52 +48,52 @@ module.exports = class App {
     //   }
     // }
 
-    // for (let y=0; y<Engine.height; y++) {
-    //   for (let x=0; x<Engine.width; x++) {
-    //     scratch.push(this.frame[y][x]);
-    //     // console.log('>>>>', scratch.length);
-    //   }
-    // }
+    for (let y=0; y<Engine.height; y++) {
+      for (let x=0; x<Engine.width; x++) {
+        scratch.push(this.frame[y][x]);
+        // console.log('>>>>', scratch.length);
+      }
+    }
 
     // //first half
     // console.log('asdf', Engine.height);
-    for (let x=Engine.width-1; x>=0; x--) {
-
-      if(x%2>0){
-        for (let y=0; y<(Engine.height/2); y++) {
-          // scratch.push(colorScale[scratch.length]);
-          console.log('>>>>', y, x, this.frame[y][x], scratch.length);
-          // console.log( scratch.length);
-          scratch.push(this.frame[y][x]);
-        }
-      }else{
-        for (let y=(Engine.height/2)-1; y>=0; y--) {
-          // scratch.push(colorScale[scratch.length]);
-          console.log('>>>>', y, x, this.frame[y][x], scratch.length);
-          // console.log( scratch.length);
-          scratch.push(this.frame[y][x]);
-        }
-      }
-    }
-    console.log('\n\n===============\n\n');
-    for (let x=Engine.width-1; x>=0; x--) {
-
-      if(x%2>0){
-        for (let y=(Engine.height/2); y<Engine.height; y++) {
-          // scratch.push(colorScale[scratch.length]);
-          console.log('>>>>', y, x, this.frame[y][x], scratch.length);
-          // console.log( scratch.length);
-          scratch.push(this.frame[y][x]);
-        }
-      }else{
-        for (let y=(Engine.height-1); y>=(Engine.height/2); y--) {
-          // scratch.push(colorScale[scratch.length]);
-          console.log('>>>>', y, x, this.frame[y][x], scratch.length);
-          // console.log( scratch.length);
-          scratch.push(this.frame[y][x]);
-        }
-      }
-    }
+    // for (let x=Engine.width-1; x>=0; x--) {
+    //
+    //   if(x%2>0){
+    //     for (let y=0; y<(Engine.height/2); y++) {
+    //       // scratch.push(colorScale[scratch.length]);
+    //       console.log('>>>>', y, x, this.frame[y][x], scratch.length);
+    //       // console.log( scratch.length);
+    //       scratch.push(this.frame[y][x]);
+    //     }
+    //   }else{
+    //     for (let y=(Engine.height/2)-1; y>=0; y--) {
+    //       // scratch.push(colorScale[scratch.length]);
+    //       console.log('>>>>', y, x, this.frame[y][x], scratch.length);
+    //       // console.log( scratch.length);
+    //       scratch.push(this.frame[y][x]);
+    //     }
+    //   }
+    // }
+    // console.log('\n\n===============\n\n');
+    // for (let x=Engine.width-1; x>=0; x--) {
+    //
+    //   if(x%2>0){
+    //     for (let y=(Engine.height/2); y<Engine.height; y++) {
+    //       // scratch.push(colorScale[scratch.length]);
+    //       console.log('>>>>', y, x, this.frame[y][x], scratch.length);
+    //       // console.log( scratch.length);
+    //       scratch.push(this.frame[y][x]);
+    //     }
+    //   }else{
+    //     for (let y=(Engine.height-1); y>=(Engine.height/2); y--) {
+    //       // scratch.push(colorScale[scratch.length]);
+    //       console.log('>>>>', y, x, this.frame[y][x], scratch.length);
+    //       // console.log( scratch.length);
+    //       scratch.push(this.frame[y][x]);
+    //     }
+    //   }
+    // }
 
     // // console.log('\n\n=========================\n\n');
     // for (let x=Engine.width-1; x>=0; x--) {
