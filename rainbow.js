4ecd047849fc429d9f627f976c413f0f5b442750
Toily <escusado@gmail.com>
2017-11-12 01:50:36 -0600

    this is it

--- rainbow.js
+++ rainbow.js
@@ -11,7 +11,7 @@ const Chroma = require('chroma-js');
 Exec('echo dale pal piso! && fcserver /usr/src/app/fcserver-config.json').stdout.pipe(process.stdout);
 
 function draw() {
-    var millis = new Date().getTime(),counter = 0, x=0,y=180, increase = 90/180*Math.PI / 10, limit=0;
+    var millis = new Date().getTime(),counter = 0, x=0,y=180, increase = 90/180*Math.PI / 100, limit=0;
 
     for (var pixel = 0; pixel < 512; pixel++)
     {
@@ -23,7 +23,7 @@ function draw() {
         // let {red,green,blue} = Chroma.hcl(Math.sin(t + 0.1), Math.sin(t + 0.2), Math.sin(t + 0.3));
         // console.log('>', red,green,blue);
 
-        y =  180 - Math.sin(counter) * 120;
+        y =  (180 - Math.sin(counter) * 120)-50;
         counter += increase;
         console.log('>>>', y);
 
@@ -34,7 +34,7 @@ function draw() {
         // var green = 128 + 96 * Math.sin(t + 0.1);
         // var blue = 128 + 96 * Math.sin(t + 0.3);
 
-        let [red,green,blue] = Chroma.hsl(y, 100, 128 + 96 * Math.sin(t + 0.1)).rgb();
+        let [red,green,blue] = Chroma(y, 1, 0.6, 'hsl').rgb();
 
         client.setPixel(pixel, red, green, blue);
     }
