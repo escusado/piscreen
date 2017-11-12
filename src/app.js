f7c60ee967ae0a5c07bce392234f2a6406656f1a
Toily <escusado@gmail.com>
2017-11-11 23:19:03 -0600

    arrange panels

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ module.exports = class App {
       this.frame.push([]);
       for (let x=0; x<Engine.width; x++) {
         // this.frame[y][x] = colorScale[colorIndex];
-        this.frame[y][x] = colorIndex % 24 ? colorScale[colorIndex] : 'blue';
+        this.frame[y][x] = colorIndex % 24 ? colorScale[colorIndex] : 'white';
         colorIndex++;
         console.log();
       }
