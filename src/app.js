5a17f2fe6b227a96222e5befce5ce3563b5b4ffe
Toily <escusado@gmail.com>
2017-11-12 01:05:27 -0600

    this is it

--- src/app.js
+++ src/app.js
@@ -11,14 +11,14 @@ module.exports = class App {
 
     const colorScale = Chroma.scale(['#0000ff', '#ff0000', '#00ff00']).mode('lch').colors(Engine.pixelCount)
     let colorIndex = 0;
+    
     for (let y=0; y<Engine.height; y++) {
-      // const currentColor = y % 2 ? 'red' : 'green';
       this.frame.push([]);
       for (let x=0; x<Engine.width; x++) {
-        // this.frame[y][x] = colorScale[colorIndex];
+        this.frame[y][x] = colorScale[colorIndex];
 
         //borders
-        this.frame[y][x] = y===8 || y===7 || x===7 || x===8 || x===16 || x===15 || y===0 || x===0 || y===(Engine.height-1) || x===(Engine.width-1) ? '#222' : colorScale[colorIndex];
+        // this.frame[y][x] = y===8 || y===7 || x===7 || x===8 || x===16 || x===15 || y===0 || x===0 || y===(Engine.height-1) || x===(Engine.width-1) ? '#222' : colorScale[colorIndex];
         // this.frame[y][x] = y===0 && x===0 ? 'red' : this.frame[y][x];
         // this.frame[y][x] = y===(Engine.height-1) && x===(Engine.width-1) ? 'blue' : this.frame[y][x];
         // this.frame[y][x] = y===0 && x===(Engine.width-1) ? 'green' : this.frame[y][x];
@@ -28,9 +28,6 @@ module.exports = class App {
         // this.frame[y][x] = (y===8 && x===7) || (y===8 && x===15) ? 'yellow' : this.frame[y][x];
         // this.frame[y][x] = (y===15 && x===7) || (y===15 && x===15) ? 'yellow' : this.frame[y][x];
 
-        // this.frame[y][x] = (x%8>0) || (y%8>0) ? 'black' : this.frame[y][x];
-
-        // this.frame[y][x] = colorIndex % 24 ? colorScale[colorIndex] : 'white';
         colorIndex++;
         console.log();
       }
