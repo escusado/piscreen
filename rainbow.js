f10ede006b91faf463e551c3f3b1a603784198fb
Toily <escusado@gmail.com>
2019-02-12 20:07:19 -0600

    adds deps

--- rainbow.js
+++ rainbow.js
@@ -5,13 +5,15 @@ var OPC = new require('./opc')
 var client = new OPC('localhost', 7890);
 const Exec = require('child_process').exec;
 
+let PIXEL_COUNT = 63;
+
 Exec('echo dale pal piso! && fcserver /usr/src/app/fcserver-config.json').stdout.pipe(process.stdout);
-const colorScale = Chroma.scale(['#0000ff', '#ff0000', '#00ff00']).mode('lch').colors(Engine.pixelCount);
+const colorScale = Chroma.scale(['#0000ff', '#ff0000', '#00ff00']).mode('lch').colors(PIXEL_COUNT);
 
 function draw() {
     var millis = new Date().getTime();
 
-    for (var pixel = 0; pixel < 63; pixel++)
+    for (var pixel = 0; pixel < PIXEL_COUNT; pixel++)
     {
         var t = pixel * 0.2 + millis * 0.002;
         var red = 128 + 96 * Math.sin(t);
